f87b0af48f4d8e036ea3ef1695e7313774921c07
Cesar Rodriguez <streamlink777@live.com>
2020-06-19 15:36:03 -0400

    finish adding links for webiamges

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ mongoose.connect('mongodb://localhost:27017/JavierAccounts', {
 });
 const postSchema = {
   name: String,
+  formatedName:String,
   info: String,
   views: Number,
   downloads: Number,
@@ -34,9 +35,11 @@ const postSchema = {
   aperture: String,
   shutterSpeed: String,
   ISO: Number,
-  dimentions: String,
   imagePath: String,
   imagelowResolutionPath: String,
+  imagePathForWeb:String,
+  width: String,
+  height:String,
 }
 const Post = mongoose.model("Post", postSchema);
 ///////////////////////////////////////////////////////////////
@@ -46,26 +49,33 @@ app.listen(3000, function () {
 });
 
 app.get('/', function (req, res) {
-  res.render('home');
+
+  Post.find({}, function(err,posts){
+    if(!err){
+      res.render('home', {posts:posts});
+    }else{
+      res.send('error');
+    }
+  });
+  
 });
 
-
 app.get('/compose', function (req, res) {
-  res.render('compose', );
+  res.render('compose');
 });
 
 app.post('/compose', function (req, res) {
   var name = _.camelCase(req.body.name);
   if (req.files) {
-    var file = req.files.imgUpload;
-    var fileName = file.name;
-
-    var rawImagePath = path.join("./uploads/images/", fileName);
-    var extentionInImage = path.extname(rawImagePath);
+    var file = req.files.imgUpload;  // file send from compose.ejs
+    var fileName = file.name;        // name on the file when is uploaded 
+  
+    var rawImagePath = path.join("./uploads/images/", fileName);  
+    var extentionInImage = path.extname(rawImagePath);                                          // this extract the extention in the file uploded 
     var imageName = path.basename(rawImagePath, extentionInImage)
-    var lowResolutionPath = "./uploads/lowResolution/" + _.camelCase(imageName) + extentionInImage;
+    var lowResolutionPath = "./public/img/forWeb/" + _.camelCase(imageName) + extentionInImage; // to save in public folder after is reduce size
+    var imagePathForWeb = "img/forWeb/" + _.camelCase(imageName) + extentionInImage;            // path to render pics in templace.
     var imagePathFormated = "./uploads/images/" + _.camelCase(imageName) + extentionInImage;
-
 
     file.mv(imagePathFormated, function (err) {
       if (!err) {
@@ -74,6 +84,7 @@ app.post('/compose', function (req, res) {
 
         const posts = new Post({
           name: req.body.name,
+          formatedName:_.camelCase(imageName),
           info: req.body.info,
           cameraMake: req.body.cameraMake,
           cameraModel: req.body.cameraModel,
@@ -83,16 +94,38 @@ app.post('/compose', function (req, res) {
           ISO: req.body.iso,
           imagePath: imagePathFormated,
           imagelowResolutionPath: lowResolutionPath,
-          dimensions: dimensions.width+'x'+ dimensions.height,
+          imagePathForWeb:imagePathForWeb,
+          width: dimensions.width,
+          height:dimensions.height,
         });
         posts.save();
       }
     });
 
-
-
     res.redirect('/compose');
   } else {
     res.send('didnt work');
   };
+});
+
+app.get('/:postid', function (req, res) {
+  const postid = req.params.postid;
+  Post.findById(postid, function(err,post){
+    if(err){
+      console.log(err);
+    }else{
+      res.render('detailPost', {post:post})
+    }
+  })
+});
+
+app.get('/detele', function(req, res){
+  Post.find({}, function(err,posts){
+    if(!err){
+      res.render('delete', {posts:posts});
+    }else{
+      res.send('error');
+    }
+  });
+
 });
