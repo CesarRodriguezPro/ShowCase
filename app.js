08dc1f2f3f92a99a203552220fc945f36fa6eed1
Cesar Rodriguez <streamlink777@live.com>
2020-06-21 13:37:08 -0400

    finish deletion section

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connect('mongodb://localhost:27017/wild-heartDB', {
 });
 const postSchema = {
   name: String,
-  formatedName:String,
+  formatedName: String,
   info: String,
   views: Number,
   downloads: Number,
@@ -38,9 +38,9 @@ const postSchema = {
   ISO: Number,
   imagePath: String,
   imagelowResolutionPath: String,
-  imagePathForWeb:String,
+  imagePathForWeb: String,
   width: String,
-  height:String,
+  height: String,
 }
 const Post = mongoose.model("Post", postSchema);
 ///////////////////////////////////////////////////////////////
@@ -51,14 +51,16 @@ app.listen(process.env.PORT || 3000, function () {
 
 app.get('/', function (req, res) {
 
-  Post.find({}, function(err,posts){
-    if(!err){
-      res.render('home', {posts:posts});
-    }else{
+  Post.find({}, function (err, posts) {
+    if (!err) {
+      res.render('home', {
+        posts: posts
+      });
+    } else {
       res.send('error');
     }
   });
-  
+
 });
 
 app.get('/compose', function (req, res) {
@@ -68,24 +70,26 @@ app.get('/compose', function (req, res) {
 app.post('/compose', function (req, res) {
   var name = _.camelCase(req.body.name);
   if (req.files) {
-    var file = req.files.imgUpload;  // file send from compose.ejs
-    var fileName = file.name;        // name on the file when is uploaded 
-  
-    var rawImagePath = path.join("./uploads/images/", fileName);  
-    var extentionInImage = path.extname(rawImagePath);                                          // this extract the extention in the file uploded 
-    var imageName = path.basename(rawImagePath, extentionInImage)
-    var lowResolutionPath = "./public/img/forWeb/" + _.camelCase(imageName) + extentionInImage; // to save in public folder after is reduce size
-    var imagePathForWeb = "img/forWeb/" + _.camelCase(imageName) + extentionInImage;            // path to render pics in templace.
-    var imagePathFormated = "./uploads/images/" + _.camelCase(imageName) + extentionInImage;
+    var file = req.files.imgUpload; // file send from compose.ejs
+    var fileName = file.name; // name on the file when is uploaded 
 
-    file.mv(imagePathFormated, function (err) {
+    var rawImagePath = path.join("./uploads/images/", fileName);
+    var extentionInImage = path.extname(rawImagePath); // this extract the extention in the file uploded 
+    var imageName = path.basename(rawImagePath, extentionInImage)
+    var lowResolutionPath = "/public/img/forWeb/" + _.camelCase(imageName) + extentionInImage; // to save in public folder after is reduce size
+    var imagePathForWeb = "img/forWeb/" + _.camelCase(imageName) + extentionInImage; // path to render pics in templace.
+    var imagePathFormated = "/uploads/images/" + _.camelCase(imageName) + extentionInImage;
+
+    file.mv("." + imagePathFormated, function (err) {
       if (!err) {
-        sharp(imagePathFormated).resize({height: 780}).toFile(lowResolutionPath);
-        var dimensions = sizeOf(imagePathFormated); 
+        sharp("." + imagePathFormated).resize({
+          height: 1000
+        }).toFile("." + lowResolutionPath);
+        var dimensions = sizeOf("." + imagePathFormated);
 
         const posts = new Post({
           name: req.body.name,
-          formatedName:_.camelCase(imageName),
+          formatedName: _.camelCase(imageName),
           info: req.body.info,
           cameraMake: req.body.cameraMake,
           cameraModel: req.body.cameraModel,
@@ -95,9 +99,9 @@ app.post('/compose', function (req, res) {
           ISO: req.body.iso,
           imagePath: imagePathFormated,
           imagelowResolutionPath: lowResolutionPath,
-          imagePathForWeb:imagePathForWeb,
+          imagePathForWeb: imagePathForWeb,
           width: dimensions.width,
-          height:dimensions.height,
+          height: dimensions.height,
         });
         posts.save();
       }
@@ -110,35 +114,45 @@ app.post('/compose', function (req, res) {
 
 app.get('/post/:postid', function (req, res) {
   const postid = req.params.postid;
-  Post.findById(postid, function(err,post){
-    if(err){
+  Post.findById(postid, function (err, post) {
+    if (err) {
       console.log(err);
-    }else{
-      res.render('detailPost', {post:post})
+    } else {
+      res.render('detailPost', {
+        post: post
+      })
     }
   })
 });
 
-app.get('/delete', function(req, res){
-  Post.find({}, function(err,posts){
-    if(!err){
-      res.render('delete', {posts:posts});
-    }else{
+app.get('/delete', function (req, res) {
+  Post.find({}, function (err, posts) {
+    if (!err) {
+      res.render('delete', {
+        posts: posts
+      });
+    } else {
       res.send('error');
     }
   });
 });
 
-app.post('/delete', function(req, res){
-  ////////////// working in this part 
-
-  const postId= req.body.postId;
-  Post.findById(postId, function(post){
-    console.log(post);
-    fs.unlink(post.imagePath)
-    fs.unlink(post.imagelowResolutionPath);
-    Post.findByIdAndDelete(postId);
-    console.log('everything was delete')
+app.post('/delete', function (req, res) {
+  const postId = req.body.postId;
+  Post.findById(postId, function (err, post) {
+    if (!err) {
+      console.log(post);
+      fs.unlink(__dirname + post.imagePath, function (err) {
+        err ? console.log(err) : console.log('images full image');
+      })
+      fs.unlink(__dirname + post.imagelowResolutionPath, function (err) {
+        err ? console.log(err) : console.log('images erase low image');
+      });
+      Post.findByIdAndDelete(postId, function (err) {
+        err ? console.log(err) : console.log('item in the database was delete.');
+      });
+      res.redirect('delete');
+      // console.log('everything was delete')
+    }
   })
-
 });
